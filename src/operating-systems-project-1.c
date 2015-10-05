294e36ffc9d5ebb5a68ba6494fe5e76d88c5b536
James Kieley <jkieley@asu.edu>
2015-10-04 18:04:12 -0700

    init project

---
+++ src/operating-systems-project-1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : operating-systems-project-1.c
+ Author      : James Kieley
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
